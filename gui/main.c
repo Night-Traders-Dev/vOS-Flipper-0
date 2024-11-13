ee3061fcaaf7b604ff2355c943f127aeedd3bd27
Night-Traders-Dev <quegmeister@gmail.com>
2024-11-13 05:32:28 -0500

    Tests

--- gui/main.c
+++ gui/main.c
@@ -9,9 +9,9 @@ static void draw_callback(Canvas* canvas, void* ctx) {
     canvas_clear(canvas);
 
     canvas_set_font(canvas, FontSecondary);
-    canvas_draw_box(canvas, 0, 0, canvas_width(canvas), 14); // Draw the status bar background
+    canvas_draw_box(canvas, 0, 0, canvas_width(canvas), 14);
     canvas_set_color(canvas, ColorWhite);
-    canvas_draw_str(canvas, 2, 10, "Status Bar"); // Example text for the status bar
+    canvas_draw_str(canvas, 2, 10, "Status Bar");
 }
 
 int32_t main_entry(void* p) {
@@ -22,7 +22,7 @@ int32_t main_entry(void* p) {
     // Initialize ViewPort
     ViewPort* view_port = view_port_alloc();
     view_port_draw_callback_set(view_port, draw_callback, NULL);
-    view_port_input_callback_set(view_port, button_input_callback, &running); // Use the new input callback
+    view_port_input_callback_set(view_port, button_input_callback, &running);
 
     Gui* gui = furi_record_open(RECORD_GUI);
     gui_add_view_port(gui, view_port, GuiLayerFullscreen);
